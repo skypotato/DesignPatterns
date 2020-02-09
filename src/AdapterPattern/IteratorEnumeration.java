3c1c3415d8ce463a4cf2609d65b17eea8ccdb7f4
SkyPotato <hunsooski@naver.com>
2020-02-10 01:09:12 +0900

    2020.02.10 : 퍼사드와 어댑터 패턴 소스

---
+++ src/AdapterPattern/IteratorEnumeration.java
@@ -0,0 +1,18 @@
+package AdapterPattern;
+import java.util.*;
+
+public class IteratorEnumeration implements Enumeration {
+	Iterator iterator;
+ 
+	public IteratorEnumeration(Iterator iterator) {
+		this.iterator = iterator;
+	}
+ 
+	public boolean hasMoreElements() {
+		return iterator.hasNext();
+	}
+ 
+	public Object nextElement() {
+		return iterator.next();
+	}
+}
---
+++ src/AdapterPattern/MallardDuck.java
@@ -0,0 +1,10 @@
+package AdapterPattern;
+public class MallardDuck implements Duck {
+	public void quack() {
+		System.out.println("Quack");
+	}
+ 
+	public void fly() {
+		System.out.println("I'm flying");
+	}
+}
