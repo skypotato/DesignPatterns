091328dc757ec4a4ed6589d1ddedeb5a3f6c7b25
skypotato <hunsooski@naver.com>
2020-03-09 22:56:00 +0900

    2020.03.08 : 프록시 첫 소스

---
+++ src/proxyPattern/GumballMachine.java
@@ -0,0 +1,17 @@
+package proxyPattern;
+
+public class GumballMachine {
+    // 기타 인스턴스 변수
+    String location;
+
+    public GumballMachine(String location, int cost){
+        // 기타 생성 코드
+        this.location = location;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    // 기타 메소드
+}
